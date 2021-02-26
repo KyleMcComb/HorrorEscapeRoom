7075e95582ca8acc2432d59d389cf78112e44f6e
oliviamallon <oliviamallon2020@gmail.com>
2021-02-26 20:31:53 +0000

    changes to exit room - clicking door (sort of works) not fully responsive yet - Css and div tags messing it up

--- js/jsExitRoom.js
+++ js/jsExitRoom.js
@@ -2,3 +2,40 @@
    document.getElementById("change").innerHTML = "hello";
    document.getElementById("change").value = "hello";
  }
+
+ //THIS IS COPIED CODE BUT I WILL CHECK THIS AND UNDERSTAND IT THEN EDIT IT MYSELF! - Olivia
+ window.onload = function () {
+ var ImageMap = function (map, img) {
+ var n,
+     areas = map.getElementsByTagName('area'),
+     len = areas.length,
+     coords = [],
+     previousWidth = 1280;
+ for (n = 0; n < len; n++) {
+     coords[n] = areas[n].coords.split(',');
+ }
+ this.resize = function () {
+     var n, m, clen,
+         x = img.offsetWidth / previousWidth;
+     for (n = 0; n < len; n++) {
+         clen = coords[n].length;
+         for (m = 0; m < clen; m++) {
+             coords[n][m] *= x;
+         }
+         areas[n].coords = coords[n].join(',');
+     }
+     previousWidth = document.body.clientWidth;
+     return true;
+ };
+ window.onresize = this.resize;
+},
+imageMap = new ImageMap(document.getElementById('map_ID'), document.getElementById('img_ID'));
+imageMap.resize();
+return;
+
+function changeText(){
+ document.getElementsById('change').color='red';
+}
+
+
+}
