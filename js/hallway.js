43c81672169199b7703105f9121e6115419d02a2
Fergus <fergusontgomery067@gmail.com>
2021-03-02 16:35:17 +0000

    changed from using maps to relative buttons, changed the column selectors in room/css to use id instead of class and renamed them to col-left and col-right. Border around buttons can be removed completely, just left them in to test easily.

--- js/hallway.js
+++ js/hallway.js
@@ -1,41 +1,27 @@
-function changeText {
-  document.getElementById("change").innerHTML = "hello";
-  document.getElementById("change").value = "hello";
-}
-
-//THIS IS COPIED CODE BUT I WILL CHECK THIS AND UNDERSTAND IT THEN EDIT IT MYSELF! - Olivia
-window.onload = function () {
-var ImageMap = function (map, img) {
-var n,
-    areas = map.getElementsByTagName('area'),
-    len = areas.length,
-    coords = [],
-    previousWidth = 1280;
-for (n = 0; n < len; n++) {
-    coords[n] = areas[n].coords.split(',');
-}
-this.resize = function () {
-    var n, m, clen,
-        x = img.offsetWidth / previousWidth;
-    for (n = 0; n < len; n++) {
-        clen = coords[n].length;
-        for (m = 0; m < clen; m++) {
-            coords[n][m] *= x;
-        }
-        areas[n].coords = coords[n].join(',');
-    }
-    previousWidth = document.body.clientWidth;
-    return true;
-};
-window.onresize = this.resize;
-},
-imageMap = new ImageMap(document.getElementById('map_ID'), document.getElementById('img_ID'));
-imageMap.resize();
-return;
-
-function changeText(){
-document.getElementsById('change').color='red';
+function changeText() {
+  document.getElementsById('change').color = 'red';
 }
 
 
+function windowClick() {
+
+  document.getElementById("output").innerHTML = "A steel barred window, far to samll to squeeze through, keep looking!";
+}
+
+function sideRoomClick() {
+  var window = document.getElementById('output').innerHTML = "";
+
+  var h = document.createElement("p");
+  var t = document.createTextNode("Hello World");
+  h.appendChild(t);
+  document.body.appendChild(h);
+}
+
+function ExitClick() {
+  var window = document.getElementById('output').innerHTML = "";
+
+  var h = document.createElement("p");
+  var t = document.createTextNode("Hello World");
+  h.appendChild(t);
+  document.body.appendChild(h);
 }
