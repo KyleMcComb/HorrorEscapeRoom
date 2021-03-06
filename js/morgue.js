c37e471f176674696f2744274d259df7eb0b8db6
Fergus <fergusontgomery067@gmail.com>
2021-03-06 19:20:25 +0000

    morgue stuff

--- js/morgue.js
+++ js/morgue.js
@@ -1,1 +1,14 @@
+var noteClicked = false;
 
+
+function expandNote(){
+  noteClicked = true;
+  document.getElementById("myModalDiv").style.display = "block";
+  document.getElementById("expandedNote").src = "img/Morgue/ChemicalNote.jpg";
+  document.getElementById("caption").innerHTML = "An old note belongning to Dr Friedrich, concerning chemical formula";
+}
+
+function closeNote() {
+  document.getElementById("myModalDiv").style.display = "none";
+  document.getElementById("wiresModal").style.display = "none";
+}
