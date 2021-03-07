600e401d452344cc0cabb248ab009eddef5b76a9
Fergus <fergusontgomery067@gmail.com>
2021-03-07 00:18:04 +0000

    progress on the morgue

--- js/morgue.js
+++ js/morgue.js
@@ -1,11 +1,52 @@
-var noteClicked = false;
+var expanded = false;
+var noteFound = false;
+
+
+function chemicalEvent(){
+
+if (noteFound === true)
+
+}
+else{
+
+}
+
+
+
+
+function showSelection() {
+  document.getElementById('chemicalEvent').style.visibility = "visible";
+}
+
+/*
+function chemicalClick() {
+
+var myDiv = document.getElementById("col-left");
+
+//Create array of options to be added
+var array = ["Volvo","Saab","Mercades","Audi"];
+
+//Create and append select list
+var selectList = document.createElement("select");
+selectList.setAttribute("id", "mySelect");
+myDiv.appendChild(selectList);
+
+//Create and append the options
+for (var i = 0; i < array.length; i++) {
+    var option = document.createElement("option");
+    option.setAttribute("value", array[i]);
+    option.text = array[i];
+    selectList.appendChild(option);
+}
+}*/
+
 
 
 function expandNote(){
-  noteClicked = true;
+  noteFound = true;
   document.getElementById("myModalDiv").style.display = "block";
   document.getElementById("expandedNote").src = "img/Morgue/ChemicalNote.jpg";
-  document.getElementById("caption").innerHTML = "An old note belongning to Dr Friedrich, concerning chemical formula";
+  document.getElementById("caption").innerHTML = "An old note belongning to Dr Friedrich, he seems to have mentioned some sort of cyper on it";
 }
 
 function closeNote() {
