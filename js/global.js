eb8a791c6c0c0a440da85c9aa33ba906ba18e036
oliviamallon <oliviamallon2020@gmail.com>
2021-03-09 10:46:57 +0000

    timer working in global.js, working for cell and exit but will interfere if you have an onload function! (also just deleted the unneccessary code for fergus, made a external css file for exit room

--- js/global.js
+++ js/global.js
@@ -1,2 +1,35 @@
 
-var winGame = sessionStorage.getItem("winGame");
+//TIMER BUT ANDREW WAY
+var timer=60;
+var timedEvent;
+var timePassed=0;
+
+window.addEventListener('load', function() { timedEvent = setInterval(countdown, 1000); });
+
+
+function stopTimer(){
+    //used to clear timer.
+    clearInterval(myTimer);
+}
+
+function countdown(){
+    timePassed+=1; //used to calc how long a user has spent in room
+    timer--; //takes off 60s
+
+    //halfway mark - displayed to user
+    if(timer==30 || (timer>29 && timer<30)){
+      document.getElementById('gameText').innerHTML+='<p>[System]: You hear a creek of floor boards, Dr Friedrich is getting closer, better hurry up!</p><br><br>';
+      document.getElementById('timer').innerHTML = timer;
+      //EDIT LATER? - used to display timer to gametext area
+    }else if(timer>0 && timer !=30){
+        document.getElementById('timer').innerHTML = timer;
+    } else {
+      //Stops timer, game over - display end screen
+        var winGame = "no";
+        sessionStorage.setItem("winGame", winGame);
+
+        //LINK TO NEXT PAGE - TIME UP - END SCREEN
+        window.location.href = 'endScreen.html';
+    }
+
+}
