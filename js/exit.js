3deb171eadc7fe5d88c1f0b244fa664fe879dfd9
oliviamallon <oliviamallon2020@gmail.com>
2021-03-15 10:44:39 +0000

    deleted unneccesary images and updated code to get purple key, note added to wall

--- js/exit.js
+++ js/exit.js
@@ -1,57 +1,50 @@
-var timePassed=0; //record how long the user spent in this room (seconds)
+var timePassed = 0; //record how long the user spent in this room (seconds)
 var code; //code entered in by user to escape
 
 var noKeys = 0; //used to check user has collected keys before escpaing
 var txt; //text to output to user
 
-var keyCode=[];
-var keyPadCode = 420;
-const pos = document.documentElement;
+var keyCode = []; //user enters in keypad to rerieve blue key
+var keyPadCode = 420; // correct keypad code
+var exitRoomCode = 420;
+var codePurpleKey = 872;
+const pos = document.documentElement; //document used for torch
 
 
-//Displays the code to unlock the final door (code=420)
-function displayCode(){
-  //records when the user activates this function (when user clicks exit door)
-  //this is being recorded to find out how long the user is in the Function
-  //prompt box stops the java timer, this calculates how long in function and takes it off remaining time
+//Displays the code to unlock the final door
+function displayCode() {
+  //records time spent in function as timer stops when prompt box is open
   var startTime = new Date();
-  //text to prompt user to enter a code in
   var textForPrompt = 'Enter the code to escape!:'
 
   code = prompt(textForPrompt);
   //records end time user has been in function - used to calc full time user spent entering in a code before closing prompt box
   var endTime = new Date();
-  //time spent in function
-  var timeSpent = endTime-startTime;
+  var timeSpent = endTime - startTime;
 
-  timeSpent = timeSpent/1000;
-  timer-=timeSpent;
+  timeSpent = timeSpent / 1000;
+  timer -= timeSpent;
   sessionStorage.setItem("exitTime", timePassed);
 
-  if(timer<=0){
+  if (timer <= 0) {
     //If the time is up - brings user to the end screen to show stats
     window.location.href = 'endScreen.html';
     //This variable is use to decide which image of the doctor to display
     var winGame = "lose";
     //sets gamePlay to lose to display correct doctor picture and Stats
-    //saves the variable as sessionStorage so the exit page can access it
     sessionStorage.setItem("winGame", winGame);
-  } else{
-    //checks if the user has entered in a code or not
+  } else {
     if (code == null || code == "") {
       txt = "[System]:User cancelled the prompt.";
       //checks for correct entering in of code AND the user has collected all 3 keys before moving on
-    } else if(code=="420" && noKeys==3){
+    } else if (code == exitRoomCode && noKeys == 3) {
       //saves the gamePlay to win to get Win Doctor picture and correct stats
       var winGame = "win";
       sessionStorage.setItem("winGame", winGame);
       window.location.href = 'endScreen.html';
-      //If the user has attempted a code and is incorrect, this checks to see if a user has the 3 Keys
-    }else if(noKeys<3){
+    } else if (noKeys < 3) {
       txt = "[System]: Hmm.. Looks like theres 3 locks, make sure to collect the keys before escaping!";
-    }
-    //else the user is told the code is incorrect
-    else {
+    } else {
       txt = "<p>[System]:That code is incorrect</p>";
     }
   }
@@ -60,168 +53,209 @@ function displayCode(){
   //timeSpent - records how long a person was in the prompt box
   document.getElementById("gameText").innerHTML += txt + ' <br><br>' + timeSpent;
 
+}
+
+//tells user code on wall as hint
+function wallCode() {
+  //var txt = "[System]: You have found a note - maybe there is a code. <br><br>";
+  var txt = "[System]: You have found a code on the wall - 420. <br><br>";
+  document.getElementById('gameText').innerHTML += txt;
+}
+
+function wallNote() {
+  var txt = "[System]: You have found a note - maybe there is a code. <br><br>";
+  document.getElementById('gameText').innerHTML += txt;
+
+  //EDIT LATER - WHAT DO YOU WANT TO DO
+  //displays close up of image to user
+  //displays modal div
+  document.getElementById("noteModal").style.display = "block";
+  //hint for users to remember code on key
+  document.getElementById("noteCaption").innerHTML = "You have found a note from one of the doctor's victims. Maybe it's a clue";
+
+}
+
+//used to tell the user coloured glass as alt tag for image mapping wasnt resizable
+function colouredGlass() {
+  var txt = "[System]: You have found coloured glass Gold, Blue, Purple - maybe this is a clue to Dr Friedrich's puzzle.<br>";
+  document.getElementById('gameText').innerHTML += txt;
+}
+
+function purpleKeyCode() {
+  var gameArea = document.getElementById('gameText');
+  var txt = "";
+  txt = '<p>Please choose a code to retrieve the purple key</p>';
+  txt += '<label for= "purpleCode">Code: </label>';
+  txt += '<input type= "text" id="purpleCode" name= "purpleCode"></input><input type="submit" value = "Submit" onclick="purpleSubmit();"/>';
+  gameArea.innerHTML += txt;
+
+  //keyFound('purple');
+}
+
+function purpleSubmit() {
+  //document.getElementById('gameText').innerHTML = "Hi Olivia";
+  var x = document.getElementById("purpleCode").value;
+  document.getElementById('gameText').innerHTML = "";
+  if(x==codePurpleKey){
+    keyFound('purple');
+  }else{
+    document.getElementById('gameText').innerHTML += "<p>[System]: Try again - wrong code! Hint: check the walls for notes! </p><br><br>";
+    //purpleKeyCode();
   }
 
-    function keyFound(keyType){
-      //adds Key - used to check if user has collected keys before entering in a code
-      noKeys+=1;
-      var x;
-      //switches depending which key found
-      switch(keyType){
-        case 'gold':
-          //shows image of gold key close up
-          expand('gold');
-          x = document.getElementById('goldKey');
-          txt = '<p>[System]: Well done, gold key found!';
-          break;
-        case 'blue':
-          //shows image of blue key close up
-          expand('blue');
-          x = document.getElementById('blueKey');
-            txt = '<p>[System]: Well done, blue key found!';
-          break;
-        case 'purple':
-          //shows image of purple key close up
-          expand('purple');
-          x = document.getElementById('purpleKey');
-            txt = '<p>[System]: Well done, purple key found!';
-          break;
-        default:
-          break;
-      }
-      //Tells user how many keys left to find/next steps
-      if(noKeys==1){
-        txt+=' 1/3 Keys found! Keep Searching! </p><br>';
-      }else if(noKeys==2){
-        txt+=' 2/3 Keys found! Keep Searching! </p><br>';
-      }else{
-        txt+='3/3 Keys found! All keys have been found, I can hear Dr Friedrich getting close! Better escape soon! </p><br>';
-      }
-      //displays output to gameText area
-      document.getElementById('gameText').innerHTML += txt;
-      //hides button that displayed key
-      x.style.display = "none";
+}
 
+//Key found - opens modal
+function keyFound(keyType) {
+  //adds Key - used to check if user has collected keys before entering in a code
+  noKeys += 1;
+  var x;
+  switch (keyType) {
+    case 'gold':
+      //shows image of gold key close up
+      expand('gold');
+      x = document.getElementById('goldKey');
+      txt = '<p>[System]: Well done, gold key found!';
+      break;
+    case 'blue':
+      //shows image of blue key close up
+      expand('blue');
+      x = document.getElementById('blueKey');
+      txt = '<p>[System]: Well done, blue key found!';
+      break;
+    case 'purple':
+      //shows image of purple key close up
+      expand('purple');
+      x = document.getElementById('purpleKey');
+      txt = '<p>[System]: Well done, purple key found!';
+      break;
+    default:
+      break;
+  }
+  //Tells user how many keys left to find/next steps
+  if (noKeys == 1) {
+    txt += ' 1/3 Keys found! Keep Searching! </p><br>';
+  } else if (noKeys == 2) {
+    txt += ' 2/3 Keys found! Keep Searching! </p><br>';
+  } else {
+    txt += '3/3 Keys found! All keys have been found, I can hear Dr Friedrich getting close! Better escape soon! </p><br>';
+  }
+  //displays output to gameText area
+  document.getElementById('gameText').innerHTML += txt;
+  document.getElementById('keys').innerHTML = 'You have found '+noKeys + '/3 keys so far!<br><br>';
+  //hides button that displayed key
+  x.style.display = "none";
+
+}
+
+//Expand the key image
+function expand(colour) {
+  //holds which image to display in modal box
+  var imageToShow;
+  //holds the alt tag for accessibility
+  var altTxt;
+  var captionKey = "";
+  //depends on which key button has been pressed
+  if (colour == 'blue') {
+    imageToShow = "img/Exit/blueKey.png";
+    captionKey = "You search the safe and find a blue key! The Key has a number - make sure to remember it!"
+    altTxt = 'Blue Key';
+  } else if (colour == 'purple') {
+    imageToShow = "img/Exit/purpleKey.png";
+    captionKey = "The Key has a number - make sure to remember it!"
+    altTxt = 'Purple Key';
+  } else if (colour == 'gold') {
+    imageToShow = "img/Exit/goldKey.png";
+    captionKey = "You lift a wooden board and find the Gold Key!"
+    altTxt = 'Gold Key';
+  }
+  //displays close up of image to user
+  //displays modal div
+  document.getElementById("keyModal").style.display = "block";
+  //changes src of image to correct key
+  document.getElementById("expandedKey").src = imageToShow;
+  //changes alt of image to correct key
+  document.getElementById("expandedKey").alt = altTxt;
+  //hint for users to remember code on key
+  document.getElementById("captionKey").innerHTML = captionKey;
+
+}
+
+//Expand the keypad image
+function expandKeypad() {
+  document.getElementById("lockModal").style.display = "block";
+  document.getElementById("expandedKeypad").src = "img/Exit/keypad.jpg";
+  document.getElementById("expandedKeypad").alt = "KeyPad";
+  document.getElementById("expandedKeypad").style.height = '50%';
+  document.getElementById("captionKeypad").innerHTML = "Enter the 3 Digit Code!";
+}
+
+//keypad entering in the Code
+function enterKeyCode(btnPressed) {
+  if (btnPressed == '0' || btnPressed == '1' || btnPressed == '2' || btnPressed == '3' || btnPressed == '4' || btnPressed == '5' || btnPressed == '6' || btnPressed == '7' || btnPressed == '8' || btnPressed == '9') {
+    document.getElementById('userEntry').innerHTML += btnPressed;
+    keyCode.push(Number(btnPressed));
+  } else if (btnPressed == 'submit') {
+    // var keyPadCode = 420; - CORRECT CODE
+    document.getElementById('userEntry').innerHTML = "";
+    var userEnter = "";
+    var lenCodeEnter = keyCode.length;
+    for (i = 0; i < lenCodeEnter; i++) {
+      //document.getElementById('gameText').innerHTML += '<p>'+ keyCode[i]+'</p>';
+      userEnter = (keyCode.pop()).toString() + userEnter;
     }
 
-    function wallCode(){
-      var txt = "[System]: You have found a number on the wall: 420, the number of Dr Friedrich's victims. I wonder could this be a code for something?<br>";
-      document.getElementById('gameText').innerHTML += txt;
+    if (lenCodeEnter > 3 || lenCodeEnter < 3) {
+      playSound('error');
+      document.getElementById("captionKeypad").innerHTML = "Enter the 3 Digit Code!<p>the code should be 3 numbers! Try Again!</p>";
+      keyCode = [];
+    } else if (lenCodeEnter == 3 && userEnter != keyPadCode) {
+      playSound('error');
+      document.getElementById("captionKeypad").innerHTML = "Enter the 3 Digit Code!<p>Wrong code! Try again (hint: Look at the walls)</p>";
+    } else if (lenCodeEnter == 3 && userEnter == keyPadCode) {
+      playSound('correct');
+      document.getElementById('keyPad').style.display = "none";
+      closeKey('lockModal');
+      keyFound('blue');
     }
+  }
+}
 
-    function colouredGlass(){
-      var txt = "[System]: You have found coloured glass Gold, Blue, Purple - maybe this is a clue to Dr Friedrich's puzzle.<br>";
-      document.getElementById('gameText').innerHTML += txt;
-    }
+// When the user clicks on <span> (x), close the note
+function closeKey(closeModal) {
+  if (closeModal == 'key') {
+    document.getElementById('keyModal').style.display = "none";
+  } else if (closeModal == 'lockModal') {
+    document.getElementById('lockModal').style.display = "none";
+    keyCode = [];
+  } else if (closeModal == 'note') {
+    document.getElementById('noteModal').style.display = "none";
+  }
 
-    //Expand the key image
-    function expand(colour){
-      //holds which image to display in modal box
-      var imageToShow;
-      //holds the alt tag for accessibility
-      var altTxt;
-      var captionKey="";
-      //depends on which key button has been pressed
-      if(colour=='blue'){
-        imageToShow = "img/Exit/blueKey.png";
-        captionKey = "You search the safe and find a blue key! The Key has a number - make sure to remember it!"
-        altTxt = 'Blue Key';
-      }else if (colour=='purple'){
-        imageToShow = "img/Exit/purpleKey.png";
-        captionKey = "The Key has a number - make sure to remember it!"
-        altTxt = 'Purple Key';
-      }else if (colour=='gold'){
-        imageToShow = "img/Exit/goldKey.png";
-        captionKey = "You lift a wooden board and find the Gold Key!"
-        altTxt = 'Gold Key';
-      }
-      //displays close up of image to user
-      //displays modal div
-      document.getElementById("myModal").style.display = "block";
-      //changes src of image to correct key
-      document.getElementById("expandedKey").src = imageToShow;
-      //changes alt of image to correct key
-      document.getElementById("expandedKey").alt = altTxt;
-      //hint for users to remember code on key
-      document.getElementById("caption").innerHTML = captionKey;
+}
 
-    }
+function moveTorch() {
+  var pos = document.documentElement;
 
+  var test = document.getElementById("imageDiv");
+  var rect = test.getBoundingClientRect();
 
-        // When the user clicks on <span> (x), close the note
-        function closeKey(closeModal) {
-          if(closeModal=='key'){
-            document.getElementById('myModal').style.display = "none";
-          }else if (closeModal=='lockModal'){
-            document.getElementById('lockModal').style.display = "none";
-            keyCode=[];
-          }
+  var viewportLeft = rect.left;
+  var viewportTop = rect.top;
 
-        }
+  var x = event.clientX; // x co-ord of Window
+  var y = event.clientY; // y co-ord of Window
 
-    //Expand the note image
-    function expandKeypad(){
-      document.getElementById("lockModal").style.display = "block";
-      document.getElementById("expandedKeypad").src = "img/Exit/keypad.jpg";
-      document.getElementById("expandedKeypad").alt = "KeyPad";
-      document.getElementById("expandedKeypad").style.height = '50%';
-      document.getElementById("captionKeypad").innerHTML = "Enter the 3 Digit Code!";
-    }
+  pos.style.setProperty('--x', (x - viewportLeft) + 'px');
+  pos.style.setProperty('--y', (y - viewportTop) + 'px');
+}
 
-
-        //keypad entering in the Code
-        function enterKeyCode(btnPressed){
-          if(btnPressed == '0' || btnPressed == '1' || btnPressed == '2' || btnPressed == '3' || btnPressed=='4' || btnPressed== '5' || btnPressed == '6' || btnPressed =='7' || btnPressed =='8' || btnPressed== '9'){
-            document.getElementById('userEntry').innerHTML +=btnPressed;
-            keyCode.push(Number(btnPressed));
-          }else if(btnPressed=='submit'){
-            // var keyPadCode = 420; - CORRECT CODE
-            document.getElementById('userEntry').innerHTML= "";
-            var userEnter="";
-            var lenCodeEnter = keyCode.length;
-            for(i=0;i<lenCodeEnter;i++){
-                  //document.getElementById('gameText').innerHTML += '<p>'+ keyCode[i]+'</p>';
-                  userEnter = (keyCode.pop()).toString() + userEnter;
-            }
-
-            if(lenCodeEnter>3 || lenCodeEnter<3){
-              playSound('error');
-                    document.getElementById("captionKeypad").innerHTML = "Enter the 3 Digit Code!<p>the code should be 3 numbers! Try Again!</p>";
-                    keyCode = [];
-            }else if (lenCodeEnter==3  && userEnter!=keyPadCode){
-              playSound('error');
-              document.getElementById("captionKeypad").innerHTML = "Enter the 3 Digit Code!<p>Wrong code! Try again (hint: Look at the walls)</p>";
-            }else if (lenCodeEnter==3  && userEnter==keyPadCode){
-             playSound('correct');
-             document.getElementById('keyPad').style.display="none";
-             closeKey('lockModal');
-             keyFound('blue');
-            }
-          }
-        }
-
-        function moveTorch(){
-          var pos = document.documentElement;
-
-          var test = document.getElementById("imageDiv");
-          var rect = test.getBoundingClientRect();
-
-          var viewportLeft = rect.left;
-          var viewportTop = rect.top;
-
-          var x = event.clientX;     // x co-ord of Window
-          var y = event.clientY;     // y co-ord of Window
-
-          pos.style.setProperty('--x',(x-viewportLeft) + 'px');
-          pos.style.setProperty('--y', (y-viewportTop) + 'px');
-        }
-
-function playSound(errorWin){
+function playSound(errorWin) {
   let sound;
-  if(errorWin =='error'){
+  if (errorWin == 'error') {
     sound = document.getElementById('error')
-  }else{
-     sound = document.getElementById('correct')
+  } else {
+    sound = document.getElementById('correct')
   }
 
   let play = document.getElementById('play')
