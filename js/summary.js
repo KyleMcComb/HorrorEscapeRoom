d8e5cad7c679e8ff0b9e20c94e37485efacba5c3
oliviamallon <oliviamallon2020@gmail.com>
2021-03-19 08:25:32 +0000

    table works to store timers in for the summary page.

--- js/summary.js
+++ js/summary.js
@@ -1,28 +1,73 @@
 function restart(){
-  sessionStorage.clear();
+  //sessionStorage.clear();
+  var rooms = ['cellTime', 'labTime','hallwayTime','securityTime','exitTime'];
+  //sets timers to 0 so user can start again with new times being set
+  for(x=0; x<5; x++){
+    sessionStorage.setItem(rooms[x], 0);
+  }
+  //sessionStorage.setItem("exitTime", 0);
   window.location.href = 'index.html';
 }
 
 function gameMode(){
   var winGame = sessionStorage.getItem("winGame");
+  var roomName = document.getElementById('roomName');
 
   if(winGame=="win"){
     document.getElementById("doctor").src = "img/EndScreen/Surgeon-Win.jpg";
-
-    var gameText = document.getElementById('roomTimes');
-
-    var cellTime = sessionStorage.getItem("cellTime");
-    gameText.innerHTML = '<li>Time in Cell Room: ' +cellTime + ' seconds</li>';
-    var labTime = sessionStorage.getItem("labTime");
-    gameText.innerHTML += '<li>Time in Laboratory Room: ' +labTime + ' seconds</li>';
-    var hallwayTime = sessionStorage.getItem("hallwayTime");
-    gameText.innerHTML += '<li>Time in Hallway Room: ' +hallwayTime + ' seconds</li>';
-    var securityTime =   sessionStorage.getItem("securityTime");
-    gameText.innerHTML += '<li>Time in Security Room: ' +securityTime + ' seconds</li>';
-    var exitTime = sessionStorage.getItem("exitTime");
-    gameText.innerHTML += '<li>Time in Final Room: ' +exitTime + ' seconds</li>';
-
+    roomName.innerHTML = 'You Won!';
   }else{
     document.getElementById("doctor").src="img/EndScreen/Surgeon-Lose.jpg";
+      roomName.innerHTML = 'You Lost!';
+  }
+
+  var cellTime = sessionStorage.getItem("cellTime");
+  var labTime = sessionStorage.getItem("labTime");
+  var hallwayTime = sessionStorage.getItem("hallwayTime");
+  var securityTime =   sessionStorage.getItem("securityTime");
+  var exitTime = sessionStorage.getItem("exitTime");
+
+
+  var tbl = document.getElementById('stats');
+  var rows = tbl.rows.length;
+
+  if(cellTime!=0 && cellTime!=null){
+    var row = tbl.insertRow(rows);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    cell1.innerHTML = "Cell Time";
+    cell2.innerHTML = cellTime;
+  }
+  rows = tbl.rows.length;
+  if(labTime!=0 && labTime!=null){
+    var row = tbl.insertRow(rows);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    cell1.innerHTML = "Lab Time";
+    cell2.innerHTML = labTime;
+  }
+  rows = tbl.rows.length;
+  if(hallwayTime!=0 && hallwayTime!=null){
+    var row = tbl.insertRow(rows);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    cell1.innerHTML = "Hallway/Morgue Time";
+    cell2.innerHTML = hallwayTime;
+  }
+  rows = tbl.rows.length;
+  if(securityTime!=0 && securityTime!=null){
+    var row = tbl.insertRow(rows);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    cell1.innerHTML = "Security Time";
+    cell2.innerHTML = securityTime;
+  }
+  rows = tbl.rows.length;
+  if(exitTime!=0 && exitTime!=null){
+    var row = tbl.insertRow(rows);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    cell1.innerHTML = "Exit Time";
+    cell2.innerHTML = exitTime;
   }
 }
